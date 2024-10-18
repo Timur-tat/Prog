68d7c44f77269e968f65cb36413f090b13850b1a
Timur-tat <timurmusin7664@gmail.com>
2024-10-18 20:17:27 +0500

    hellow word

---
+++ src/ABC.java
@@ -0,0 +1,56 @@
+public class ABC {
+    public static void main(String[] args) {
+        Cat cat=new Cat("Рыжик",15);
+        System.out.println(cat);
+    }
+}
+class Cat{
+    String name;
+
+    public Cat() {
+        super();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    int age;
+}
